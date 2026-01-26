e98dbc8471032daa6eba0a8cfcb082057213f957
Ahmed-97 <blejahmed@gmail.com>
2026-01-26 22:09:52 +0100

    first auth endpoint draft

---
+++ backend/endpoints/auth.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Cookie, Depends, HTTPException, Response
+import boto3
+from backend.db.db import get_db
+from backend.pydantic_models.auth_models import ConfirmSignupRequest, LoginRequest, SignupRequest
+from backend.secret_keys import SecretKeys
+from sqlalchemy.orm import Session
+
+
+router = APIRouter()
+secret_keys = SecretKeys()
+
+COGNITO_CLIENT_ID = secret_keys.COGNITO_CLIENT_ID
+COGNITO_CLIENT_SECRET = secret_keys.COGNITO_CLIENT_SECRET
+
+cognito_client = boto3.client(
+    "cognito-idp",
+    region_name=secret_keys.REGION_NAME
+)
+
+
+@router.post("/signup")
+def signup_user(
+    data: SignupRequest,
+    db: Session = Depends(get_db)
+):
+
+    try:
+        secret_hash = ""  # fixme
+
+        cognito_response = cognito_client.sign_up(
+            ClientId=COGNITO_CLIENT_ID,
+            Username=data.email,
+            Password=data.password,
+            SecretHash=secret_hash,
+            UserAttributes=[
+                {"Name": "email", "Value": data.email},
+                {"Name": "name", "Value": data.name}
+            ],
+        )
+
+        cognito_sub = cognito_response.get("UserSub")
+
+        if not cognito_sub:
+            raise HTTPException(400, "Cognito did not return a valid user sub")
+
+        new_user = ""
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+    except Exception as e:
+        raise HTTPException(400, f'Cognito signup exception: {e}')
